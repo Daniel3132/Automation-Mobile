c4d4520ce060db064b7bc89e29a308f9e80e8c2b
Daniel Correa <91207654+Daniel3132@users.noreply.github.com>
2022-11-19 15:33:37 -0500

    add locators for navigation test

--- src/test/java/screens/DashBoardScreen.java
+++ src/test/java/screens/DashBoardScreen.java
@@ -3,10 +3,7 @@ package screens;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.HowToUseLocators;
 import util.screens.BaseScreen;
-
-import static io.appium.java_client.pagefactory.LocatorGroupStrategy.ALL_POSSIBLE;
 
 /**
  * DashBoard screen.
@@ -19,7 +16,7 @@ public class DashBoardScreen extends BaseScreen {
     private AndroidElement dismissPreferenceUpdateButton;
     @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"dismiss-icon\")")
     private AndroidElement dismissWelcome;
-    @HowToUseLocators(androidAutomation = ALL_POSSIBLE)
+
     @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").description(\"Map, Tab, 2of5\")")
     private AndroidElement mapButton;
 
